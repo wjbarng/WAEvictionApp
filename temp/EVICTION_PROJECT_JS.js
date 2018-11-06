611c7a53ef56690d3386faf97ccb26cc0f084141
Wonjo Barng <wjbarng@uw.edu>
2018-11-05 21:15:38 -0800

    edit

--- temp/EVICTION_PROJECT_JS.js
+++ temp/EVICTION_PROJECT_JS.js
@@ -6,7 +6,7 @@ window.onload = function(e) {
 	document.getElementById("input_file_name").value = "eviction_notice";
 }
 
-document.getElementById("submit_button").addEventListener('click', function() {
+document.getElementById("submit").addEventListener('click', function() {
     var name = document.getElementById("input_name").innerHTML;
     var Address = document.getElementById("input_address").innerHTML;
     var County = document.getElementById("input_county").innerHTML;
@@ -14,5 +14,19 @@ document.getElementById("submit_button").addEventListener('click', function() {
     var filename = document.getElementById("input_file_name").innerHTML;
     var page = document.getElementById("input_page_number").innerHTML;
     alert("thank you");
-    window.location.href = "index.html";
+    document.getElementById("newData").style.display = "none";
+    document.getElementById("pdf").style.display = "none";
+    
 })
+
+function view(input) {
+    if(input == "newData") {
+        document.getElementById("editData").style.display = "none";
+        document.getElementById("newData").style.display = "inline-block";
+        document.getElementById("pdf").style.display = "inline-block";
+    } else {
+        document.getElementById("newData").style.display = "none";
+        document.getElementById("pdf").style.display = "none";
+        document.getElementById("editData").style.display = "inline-block";
+    }
+}
