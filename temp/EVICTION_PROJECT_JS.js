c7c51d4855e348c20f67932ca599c9fa072c695b
hjw9673 <hjw9673@gmail.com>
2018-11-05 17:00:31 -0800

    gochuhra

---
+++ temp/EVICTION_PROJECT_JS.js
@@ -0,0 +1,17 @@
+'use strict';
+
+window.onload = function(e) {
+    var pdf = document.getElementById("pdf_file");
+    pdf.src = "eviction_notice.pdf";
+}
+
+document.getElementById("submit_button").addEventListener('click', function() {
+    var name = document.getElementById("input_name").innerHTML;
+    var Address = document.getElementById("input_address").innerHTML;
+    var County = document.getElementById("input_county").innerHTML;
+    var year = document.getElementById("input_year").innerHTML;
+    var filename = document.getElementById("input_file_name").innerHTML;
+    var page = document.getElementById("input_page_number").innerHTML;
+    alert("thank you");
+    window.location.href = "index.html";
+})
