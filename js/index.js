e996615f53199694759c3c76a30f765a246caea1
wjbarng <wjbarng@gmail.com>
2018-11-05 16:44:29 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
-console.log("hi");
+'use strict';
 
-document.getElementById('submit').addEventListener('click', function() {
+window.onload = function(e) {
+    var pdf = document.getElementById("pdf_file");
+    pdf.src = "pdf/eviction_notice.pdf";
+}
+
+document.getElementById("submit_button").addEventListener('click', function() {
+    var name = document.getElementById("input_name").innerHTML;
+    var Address = document.getElementById("input_address").innerHTML;
+    var County = document.getElementById("input_county").innerHTML;
+    var year = document.getElementById("input_year").innerHTML;
+    var filename = document.getElementById("input_file_name").innerHTML;
+    var page = document.getElementById("input_page_number").innerHTML;
     alert("thank you");
+    window.location.href = "index.html";
 })
